7cadb3ab35d957a55e31a2af6034f95022c75902
FerAjset-G3 <fernando_ajset@hotmail.com>
2022-07-17 11:50:49 -0600

    Fernando Ajset : Removido menú en navbar.

---
+++ Usuarios/Models/ModificarViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Models
+{
+    public class ModificarViewModel
+    {
+        [Remote(action: "NombreExiste", controller: "Empleado")]
+        public string CodigoEmpleado { get; set; }
+    }
+}
